eada8e8b861b7935f5b5c54d58f7f0b51528ba4b
bhuva.d <bhuva.d@wehi.edu.au>
2022-01-21 16:20:55 +1100

    Added constructors for SingleCellExperimentList and RangedSummarizedExperimentList

--- R/ExperimentList.R
+++ R/ExperimentList.R
@@ -1,4 +1,6 @@
-#' @param experiments a named list of [SummarizedExperiment] objects.
+#' @param experiments a named list of [SummarizedExperiment],
+#'   [RangedSummarizedExperiment], [SingleCellExperiment], or
+#'   [SpatialExperiment] objects.
 #' @param experimentData a [DataFrame] with experiment specific annotations. The
 #'   number of rows in the data frame should be equal to the number of
 #'   experiments. Row names of the data frame should match names of experiments
@@ -17,7 +19,8 @@ SummarizedExperimentList <- function(experiments = SimpleList(),
 
   #no experiment
   if (length(experiments) == 0) {
-    return(.SummarizedExperimentList())
+    se = SummarizedExperiment::SummarizedExperiment()
+    return(.SummarizedExperimentList(se))
   }
 
   #change colnames
@@ -60,3 +63,115 @@ SummarizedExperimentList <- function(experiments = SimpleList(),
   )
 }
 
+#' @inheritParams SummarizedExperimentList
+#' @export
+RangedSummarizedExperimentList <-
+  function(experiments = SimpleList(),
+           experimentData = DataFrame(),
+           check.names = TRUE,
+           change.names = TRUE) {
+
+  stopifnot(length(experiments) == 0 | !is.null(names(experiments)))
+
+  #no experiment
+  if (length(experiments) == 0) {
+    gr = GenomicRanges::GRangesList()
+    se = SummarizedExperiment::SummarizedExperiment(rowRanges = gr)
+    return(.RangedSummarizedExperimentList(se))
+  }
+
+  #change colnames
+  experimentNames = names(experiments)
+  if (change.names) {
+    experiments = mapply(function(x, y) {
+      #if colnames present, change
+      if (!is.null(colnames(x)))
+        colnames(x) = paste(y, colnames(x), sep = '.')
+      return(x)
+    }, experiments, experimentNames, SIMPLIFY = FALSE)
+  }
+
+  #create experimentData if missing
+  if (nrow(experimentData) == 0) {
+    experimentData = S4Vectors::DataFrame(row.names = experimentNames)
+  } else {
+    stopifnot(length(experiments) == nrow(experimentData))
+  }
+
+  #check experimentData
+  if (check.names) {
+    stopifnot(all(names(experiments) %in% rownames(experimentData)))
+  } else {
+    stopifnot(length(experiments) == nrow(experimentData))
+  }
+
+  #combine SEs
+  se = do.call(SummarizedExperiment::cbind, experiments)
+  if (is(experimentData, 'data.frame'))
+    experimentData = as(experimentData, 'DataFrame')
+
+  #create reference map
+  experimentIndex = rep(1:length(experiments), times = sapply(experiments, ncol))
+
+  .RangedSummarizedExperimentList(
+    se,
+    experimentData = experimentData,
+    experimentIndex = experimentIndex
+  )
+}
+
+#' @inheritParams SummarizedExperimentList
+#' @export
+SingleCellExperimentList <-
+  function(experiments = SimpleList(),
+           experimentData = DataFrame(),
+           check.names = TRUE,
+           change.names = TRUE) {
+
+  stopifnot(length(experiments) == 0 | !is.null(names(experiments)))
+
+  #no experiment
+  if (length(experiments) == 0) {
+    sce = SingleCellExperiment::SingleCellExperiment()
+    return(.SingleCellExperimentList(sce))
+  }
+
+  #change colnames
+  experimentNames = names(experiments)
+  if (change.names) {
+    experiments = mapply(function(x, y) {
+      #if colnames present, change
+      if (!is.null(colnames(x)))
+        colnames(x) = paste(y, colnames(x), sep = '.')
+      return(x)
+    }, experiments, experimentNames, SIMPLIFY = FALSE)
+  }
+
+  #create experimentData if missing
+  if (nrow(experimentData) == 0) {
+    experimentData = S4Vectors::DataFrame(row.names = experimentNames)
+  } else {
+    stopifnot(length(experiments) == nrow(experimentData))
+  }
+
+  #check experimentData
+  if (check.names) {
+    stopifnot(all(names(experiments) %in% rownames(experimentData)))
+  } else {
+    stopifnot(length(experiments) == nrow(experimentData))
+  }
+
+  #combine SEs
+  sce = do.call(SingleCellExperiment::cbind, experiments)
+  if (is(experimentData, 'data.frame'))
+    experimentData = as(experimentData, 'DataFrame')
+
+  #create reference map
+  experimentIndex = rep(1:length(experiments), times = sapply(experiments, ncol))
+
+  .SingleCellExperimentList(
+    sce,
+    experimentData = experimentData,
+    experimentIndex = experimentIndex
+  )
+}
--- tests/testthat/test-ExperimentList.R
+++ tests/testthat/test-ExperimentList.R
@@ -1,7 +1,5 @@
-test_that("Constructor works", {
+test_that("Constructor works (SummarizedExperiment)", {
   library(SummarizedExperiment)
-  library(SingleCellExperiment)
-  library(SpatialExperiment)
 
   #empty object
   expect_true(validObject(.SummarizedExperimentList())) #internal
@@ -42,11 +40,93 @@ test_that("Constructor works", {
   se2 = SummarizedExperiment(assays = list(mat))
   se_list = list('SE1' = se1, 'SE2' = se2)
   expect_true(validObject(SummarizedExperimentList(se_list)))
+})
 
-  # expect_true(validObject(.RangedSummarizedExperimentList())) #internal
-  # expect_true(validObject(RangedSummarizedExperimentList())) #exported
+test_that("Constructor works (RangedSummarizedExperiment)", {
+  library(SummarizedExperiment)
+
+  #empty object
+  expect_true(validObject(.RangedSummarizedExperimentList())) #internal
+  expect_true(validObject(RangedSummarizedExperimentList())) #exported
+
+  #empty objects in the list
+  se_empty1 = SummarizedExperiment(rowRanges = GRangesList())
+  se_empty2 = SummarizedExperiment(rowRanges = GRangesList())
+  se_empty_list = list(se_empty1, se_empty2)
+  expect_error(RangedSummarizedExperimentList(se_empty_list))
+  names(se_empty_list) = LETTERS[1:2]
+  expect_true(validObject(RangedSummarizedExperimentList(se_empty_list)))
+
+  #with annotations
+  edata = data.frame('ID' = 1:3, row.names = letters[1:3])
+  expect_error(RangedSummarizedExperimentList(se_empty_list, experimentData = edata))
+
+  edata = edata[1:2, , drop = FALSE]
+  expect_error(RangedSummarizedExperimentList(se_empty_list, experimentData = edata))
+
+  rownames(edata) = NULL
+  expect_error(RangedSummarizedExperimentList(se_empty_list, experimentData = edata))
+  expect_true(validObject(RangedSummarizedExperimentList(se_empty_list, experimentData = edata, check.names = FALSE)))
+
+  rownames(edata) = LETTERS[1:2]
+  expect_true(validObject(RangedSummarizedExperimentList(se_empty_list, experimentData = edata)))
+
+  #non-empty data
+  mat = matrix(1, 3, 3)
+  gr = GRanges("chr2", IRanges(3:5, 6:8))
+  se1 = SummarizedExperiment(assays = list(mat), rowRanges = gr)
+  se2 = SummarizedExperiment(assays = list(mat), rowRanges = gr)
+  se_list = list('SE1' = se1, 'SE2' = se2)
+  expect_true(validObject(RangedSummarizedExperimentList(se_list)))
+
+  colnames(mat) = LETTERS[1:3]
+  rownames(mat) = letters[1:3]
+  se1 = SummarizedExperiment(assays = list(mat), rowRanges = gr)
+  se2 = SummarizedExperiment(assays = list(mat), rowRanges = gr)
+  se_list = list('SE1' = se1, 'SE2' = se2)
+  expect_true(validObject(RangedSummarizedExperimentList(se_list)))
+})
+
+test_that("Constructor works (SingleCellExperiment)", {
+  library(SingleCellExperiment)
+
+  #empty object
   # expect_true(validObject(.SingleCellExperimentList())) #internal
-  # expect_true(validObject(SingleCellExperimentList())) #exported
-  # expect_true(validObject(.SpatialExperimentList())) #internal
-  # expect_true(validObject(SpatialExperimentList())) #exported
+  expect_true(validObject(SingleCellExperimentList())) #exported
+
+  #empty objects in the list
+  sce_empty1 = SingleCellExperiment()
+  sce_empty2 = SingleCellExperiment()
+  sce_empty_list = list(sce_empty1, sce_empty2)
+  expect_error(SingleCellExperimentList(sce_empty_list))
+  names(sce_empty_list) = LETTERS[1:2]
+  expect_true(validObject(SingleCellExperimentList(sce_empty_list)))
+
+  #with annotations
+  edata = data.frame('ID' = 1:3, row.names = letters[1:3])
+  expect_error(SingleCellExperimentList(sce_empty_list, experimentData = edata))
+
+  edata = edata[1:2, , drop = FALSE]
+  expect_error(SingleCellExperimentList(sce_empty_list, experimentData = edata))
+
+  rownames(edata) = NULL
+  expect_error(SingleCellExperimentList(sce_empty_list, experimentData = edata))
+  expect_true(validObject(SingleCellExperimentList(sce_empty_list, experimentData = edata, check.names = FALSE)))
+
+  rownames(edata) = LETTERS[1:2]
+  expect_true(validObject(SingleCellExperimentList(sce_empty_list, experimentData = edata)))
+
+  #non-empty data
+  mat = matrix(1, 3, 3)
+  sce1 = SingleCellExperiment(assays = list(mat))
+  sce2 = SingleCellExperiment(assays = list(mat))
+  sce_list = list('SCE1' = sce1, 'SCE2' = sce2)
+  expect_true(validObject(SingleCellExperimentList(sce_list)))
+
+  colnames(mat) = LETTERS[1:3]
+  rownames(mat) = letters[1:3]
+  sce1 = SingleCellExperiment(assays = list(mat))
+  sce2 = SingleCellExperiment(assays = list(mat))
+  sce_list = list('SCE1' = sce1, 'SCE2' = sce2)
+  expect_true(validObject(SingleCellExperimentList(sce_list)))
 })
