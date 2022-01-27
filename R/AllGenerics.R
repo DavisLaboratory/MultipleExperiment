641fc35ea87c1596fb670ddba80322be1e525167
bhuva.d <bhuva.d@wehi.edu.au>
2022-01-27 16:21:12 +1100

    Defined Generic functions

---
+++ R/AllGenerics.R
@@ -0,0 +1,20 @@
+#' @export
+setGeneric("experiments", function(x, ...) standardGeneric("experiments"))
+
+#' @export
+setGeneric("experiments<-", function(x, ...) standardGeneric("experiments<-"))
+
+#' @export
+setGeneric("experimentNames", function(x, ...) standardGeneric("experimentNames"))
+
+#' @export
+setGeneric("experimentNames<-", function(x, ...) standardGeneric("experimentNames<-"))
+
+#' @export
+setGeneric("experimentData", function(x, ...) standardGeneric("experimentData"))
+
+#' @export
+setGeneric("experimentData<-", function(x, ...) standardGeneric("experimentData<-"))
+
+#' @export
+setGeneric("elapply", function(x, ...) standardGeneric("elapply"))
