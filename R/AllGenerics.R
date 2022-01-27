efbe839c11817c5b767e16f566ceec31a05d2528
bhuva.d <bhuva.d@wehi.edu.au>
2022-01-27 18:05:40 +1100

    Correct replace generics

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -2,19 +2,19 @@
 setGeneric("experiments", function(x, ...) standardGeneric("experiments"))
 
 #' @export
-setGeneric("experiments<-", function(x, ...) standardGeneric("experiments<-"))
+setGeneric("experiments<-", function(x, ..., value) standardGeneric("experiments<-"))
 
 #' @export
 setGeneric("experimentNames", function(x, ...) standardGeneric("experimentNames"))
 
 #' @export
-setGeneric("experimentNames<-", function(x, ...) standardGeneric("experimentNames<-"))
+setGeneric("experimentNames<-", function(x, ..., value) standardGeneric("experimentNames<-"))
 
 #' @export
 setGeneric("experimentData", function(x, ...) standardGeneric("experimentData"))
 
 #' @export
-setGeneric("experimentData<-", function(x, ...) standardGeneric("experimentData<-"))
+setGeneric("experimentData<-", function(x, ..., value) standardGeneric("experimentData<-"))
 
 #' @export
 setGeneric("elapply", function(x, ...) standardGeneric("elapply"))
