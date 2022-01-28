de0a105f2ee18faf128c30cd253591cfa98d7ff3
bhuva.d <bhuva.d@wehi.edu.au>
2022-01-28 13:13:46 +1100

    Show functions

---
+++ R/ELShow.R
@@ -0,0 +1,22 @@
+#' @importFrom S4Vectors coolcat
+#' @importFrom methods callNextMethod
+.showEL <- function(object) {
+  ## header
+  classLineage = is(object)
+  cat(classLineage[1], ' with ', nexp(object), ' ', classLineage[2], 's\n', sep = '')
+
+  ## Experiment data
+  callNextMethod()
+
+  ## experimentData()
+  cat('experiments: ', nexp(object), '\n', sep = '')
+  coolcat('experimentNames (%d): %s\n', experimentNames(object))
+  coolcat('experimentData names (%d): %s\n',colnames(experimentData(object)))
+
+}
+
+#' @export
+setMethod("show", "SummarizedExperimentList", .showEL)
+setMethod("show", "RangedSummarizedExperimentList", .showEL)
+setMethod("show", "SingleCellExperimentList", .showEL)
+setMethod("show", "SpatialExperimentList", .showEL)
