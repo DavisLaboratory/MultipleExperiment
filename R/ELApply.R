cde14a7396256aa1cc51a4b6719dfd16e3716396
bhuva.d <bhuva.d@wehi.edu.au>
2022-03-11 16:30:19 +1100

    Update apply function for empty EList

--- R/ELApply.R
+++ R/ELApply.R
@@ -1,14 +1,14 @@
 #' @rdname ExperimentList-methods
 #' @export
 setMethod("elapply", "ExperimentList", function(x, FUN, ..., check.names = TRUE, change.names = TRUE) {
-  experimentData = experimentData(x)
   FUN = match.fun(FUN)
+
+  if (nexp(x) == 0)
+    return(list())
+  experimentData = experimentData(x)
 
   #apply function
   results = lapply(experiments(x), FUN, ...)
-
-  if (length(results) == 0)
-    return(list())
 
   #simplify
   if (all(sapply(results, is, is(x)[2]))) {
