692f3b03e134ec2d9a7ad6a706dfe481f2aa5b7a
bhuva.d <bhuva.d@wehi.edu.au>
2022-03-11 15:40:04 +1100

    Update test case

--- tests/testthat/test-ELApply.R
+++ tests/testthat/test-ELApply.R
@@ -4,7 +4,7 @@ test_that("ExperimentList apply works", {
   expdf = matrix(1, 2, 1, dimnames = list(LETTERS[1:2], c('Col1')))
   spe_list = ExperimentList(list('A' = spe, 'B' = spe), expdf)
 
-  expect_equal(elapply(ExperimentList(), length), list())
+  expect_equal(length(elapply(ExperimentList(), length)), 0)
   expect_s4_class(elapply(spe_list[,, exp = 1], function(x) x[, 1]), 'ExperimentList')
   expect_equal(elapply(spe_list, ncol), list('A' = 3, 'B' = 3))
   expect_true(is(elapply(spe_list, dim), 'list'))
